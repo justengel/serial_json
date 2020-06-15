bcd8b4f3cfbd3569677c5bd64b94839972e8ce4b
justengel <jtengel08@gmail.com>
2020-06-15 19:00:49 -0400

    Updated to version 1.2.2

--- serial_json/__meta__.py
+++ serial_json/__meta__.py
@@ -1,5 +1,5 @@
 name = 'serial_json'
-version = '1.2.1'
+version = '1.2.2'
 description = 'Custom json serialization support.'
 url = 'https://github.com/justengel/serial_json'
 author = 'Justin Engel'
